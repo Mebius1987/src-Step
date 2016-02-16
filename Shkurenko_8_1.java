93a7cf08ec478ae0a8f3f96e9ffa3dd30d7f5322
Andrey Shkurenko <mebius1987@gmail.com>
2016-02-16 14:15:21 +0200

    3

--- Shkurenko_8_1.java
+++ Shkurenko_8_1.java
@@ -1,10 +1,13 @@
 package Step;
+
+import java.util.Scanner;
+
 /**
  * Created by Mebius on 02.02.2016.
  */
 public class Shkurenko_8_1 {
     public static void main(String[] args) {
-        task1();
+        task2();
     }
 
     /*
@@ -19,13 +22,42 @@ public class Shkurenko_8_1 {
 
         1. Дано число n, значение которого меньше 32. Покажите число 2n, то есть число, у которого энный бит равен 1, а все остальные – по нулям.
 */
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int n = sc.nextInt();
+        int a = 1;
+        a <<= n;
+        System.out.println("ответ = " + a);
     }
+
+    public static void task2() {
 /*
         2. Даны два неравных числа: n и m, не превосходящие 31. Вычислите 2n + 2m (да, здесь можно использовать операцию + ).
+        */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int n = sc.nextInt();
+        System.out.println("Введите число");
+        int m = sc.nextInt();
+        int otvet = 0;
+        otvet = 1 << n | 1 << m;
+        System.out.println("ответ = " + otvet);
+    }
 
+    public static void task3() {
+/*
         3. Дано целое число A и натуральное число n. Обнулите у числа A его последние n бит, и выведите результат на экран консоли.
+*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите целое число A");
+        int a = sc.nextInt();
+        System.out.println("Введите натуральное число n");
+        double n = sc.nextInt();
+        int otvet = 0;
 
+        System.out.println("ответ = " + otvet);
+    }
+/*
         4. Дано целое число A и натуральное число n. Выведите число, которое получается из числа A установкой значения n-го бита в единицу.
 
         5. Дано целое число A и натуральное число n. Выведите число, которое получается из числа A инвертированием n-го бита.
