fd299f3465e5a2bde1dfa6262db1b46bd1713190
Andrey Shkurenko <mebius1987@gmail.com>
2016-03-10 13:17:00 +0200

    1

---
+++ metodi_home_work/lini9.java
@@ -0,0 +1,26 @@
+package Step.metodi_home_work;
+
+/**
+ * Created by Mebius on 10.03.2016.
+ */
+public class lini9 {
+    public static void myLine(int length, char symbol, boolean c) {
+           /*
+        1.	Написать статический метод myLine, который можно будет вызвать так:
+        myLine(20, '@', true);
+        и при этом на экране консоли нарисуется горизонтальная линия, состоящая из 20 «собачек».
+        Если передать в последнем параметре false – линия станет вертикальной.
+*/
+
+        for (int i = 0; i < length; i++) {
+            if (c) {
+                System.out.print(symbol);
+            }
+            System.out.println(symbol);
+        }
+    }
+    public static void main(String[] args) {
+
+        myLine(20, '@', false);
+    }
+}
