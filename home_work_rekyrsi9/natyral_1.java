97b5a3bc77bb10b9556c1c8b82807d1477c785ad
Andrey Shkurenko <mebius1987@gmail.com>
2016-03-13 21:43:16 +0200

    rekyrsi9

--- home_work_rekyrsi9/natyral_1.java
+++ home_work_rekyrsi9/natyral_1.java
@@ -4,6 +4,14 @@ package Step.home_work_rekyrsi9;
  * Created by Mebius on 13.03.2016.
  */
 public class natyral_1 {
+    /*
+    1. От 1 до n.
+Дано натуральное число n. Выведите все числа от 1 до n.
+Ввод: 5
+Вывод: 1 2 3 4 5
+
+    */
+
     public static int N(int n) {
         if (n == 0 )
             return 1;//условие выхода из рекурсии
---
+++ home_work_rekyrsi9/ot_A_do_B.java
@@ -0,0 +1,7 @@
+package Step.home_work_rekyrsi9;
+
+/**
+ * Created by Mebius on 13.03.2016.
+ */
+public class ot_A_do_B {
+}
