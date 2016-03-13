46a361debaeaacdd798634252f72f2603fe3b790
Andrey Shkurenko <mebius1987@gmail.com>
2016-03-13 22:58:53 +0200

    rekyrsi9

---
+++ home_work_rekyrsi9/Fibona4i_9.java
@@ -0,0 +1,33 @@
+package Step.home_work_rekyrsi9;
+
+/**
+ * Created by Mebius on 13.03.2016.
+ */
+/*
+9. Фибоначчи.
+Определить статический метод для вычисления элементов ряда чисел Фибоначчи, исходя из рекуррентного выражения F(n) = F(n-1) + F(n-2).
+Метод принимает индекс числа, возвращает значение числа.
+
+ */
+public class Fibona4i_9 {
+    //public static int count =0;
+    public static int F(int n) {
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        else{
+
+            return F(n - 1) + F(n - 2);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(F(41));
+    }
+
+}
--- home_work_rekyrsi9/ot_A_do_B.java
+++ home_work_rekyrsi9/ot_A_do_B.java
@@ -14,24 +14,13 @@ public class ot_A_do_B {
             */
 
     public static int N(int n, int b) {
-        if (n < b) {
-            if (n == b + 1)
-                return 1;//условие выхода из рекурсии
-            System.out.println(n);
-            return N(n + 1, b);// условие входа
-        } else if (n > b) {
-
-            if (n == b)
-                return 1;//условие выхода из рекурсии
-            System.out.println(n);
-            return N(n -1, b);// условие входа
-        }
-        return N(n, b);// условие входа
+        if (n < b + 1)
+            return 1;//условие выхода из рекурсии
+        System.out.println(n);
+        return N(n + 1, b);// условие входа
     }
 
-
     public static void main(String[] args) {
-
-        N(16, 5);
+        N(1, 15);
     }
 }
