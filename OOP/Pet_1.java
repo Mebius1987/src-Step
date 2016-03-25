542ddcc8179b539e8134f76f56e9284721ad311b
Andrey Shkurenko <mebius1987@gmail.com>
2016-03-25 15:37:09 +0200

    12

---
+++ OOP/Pet_1.java
@@ -0,0 +1,57 @@
+package Step.OOP;
+
+/**
+ * Created by Mebius on 25.03.2016.
+ */
+/*
+1.	Реализовать класс Pet, который описывает характеристики и поведение домашнего питомца (собаки, рыбки, хомячка, удава, и тп).
+ */
+
+class Dog { // do not change class name!
+
+    // поля:
+    String name; // кличка собачки
+    String color; // цвет (масть)
+    String poroda;
+    int age; // возраст в годах
+    double weight; // вес
+    boolean isHungry; // голодный или нет?
+    boolean xo4etGulat;
+    boolean xo4etCesat;
+
+    // методы:
+    void play() {
+        System.out.println(name + " играется!");
+    }
+
+    void sleep() {
+        System.out.println(name + " спит.");
+    }
+
+    void laet() {
+        System.out.println(name + " лает на прохожих...");
+    }
+    void prosit(){
+        System.out.println(name + " просится гулять");
+    }
+    void gdet(){
+        System.out.println(name + " ждет хозяйку у входа");
+    }
+    void grustit(){
+        System.out.println(name + " грустит, что хозяйка не его чухает");
+    }
+}
+
+class yourMainClassName { // change!
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.name = " собачка Гарри";
+        dog.play();
+        dog.laet();
+        dog.sleep();
+        dog.prosit();
+        dog.gdet();
+        dog.grustit();
+    }
+}
